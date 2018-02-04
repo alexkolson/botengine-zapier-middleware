84221ba0247388221fd4f8b5e7e952750085fcd9
Alex Olson <alexkellenolson@gmail.com>
2018-02-04 20:36:26 +0100

    Initial webhook verification flow.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const request = require('request');
+
+module.exports = function middleware(hook) {
+  const {
+    env: {
+      BOTENGINE_VERIFICATION_TOKEN,
+    },
+    params: {
+      challenge,
+      token,
+    },
+    req: {
+      headers,
+    },
+    res,
+  } = hook;
+
+  if (challenge && token) {
+    res.setHeader('Content-Type', 'text/plain');
+    if (token !== BOTENGINE_VERIFICATION_TOKEN) {
+      res.statusCode = 401;
+      const message = '"Invalid botengine.ai verification token."';
+      console.log(message);
+      return res.end(message);
+    }
+
+    res.statusCode = 200;
+    console.log('returning challenge to botengine.ai');
+    const challengeResponse = `"${challenge}"`;
+    console.log({
+      challengeResponse,
+    });
+    return res.end(challengeResponse);
+  }
+}
