397a907448754de1afd8ac6259488205f7a796d3
Alex Olson <alexkellenolson@gmail.com>
2018-02-05 23:47:15 +0100

    Use HTTP method to determine if we need to process a verification request.

--- index.js
+++ index.js
@@ -7,31 +7,55 @@ module.exports = function middleware(hook) {
     env: {
       BOTENGINE_VERIFICATION_TOKEN,
     },
-    params: {
-      challenge,
-      token,
-    },
+    params,
     req: {
       headers,
+      method: reqMethod,
+      body,
     },
     res,
   } = hook;
 
-  if (challenge && token) {
+  // botengine.ai invokes webhook verification requests with an HTTP GET method.
+  if (reqMethod === 'GET') {
+    const {
+      challenge,
+      token,
+    } = params;
+
+    console.log({
+      message: 'Processing botengine.ai verification request.',
+      challenge,
+    });
+
     res.setHeader('Content-Type', 'text/plain');
+
+
     if (token !== BOTENGINE_VERIFICATION_TOKEN) {
       res.statusCode = 401;
-      const message = '"Invalid botengine.ai verification token."';
-      console.log(message);
-      return res.end(message);
+      const error = '"Invalid botengine.ai verification token."';
+      console.log({ error });
+      return res.end(error);
     }
 
     res.statusCode = 200;
-    console.log('returning challenge to botengine.ai');
+
     const challengeResponse = `"${challenge}"`;
+
     console.log({
+      message: 'Returning challenge response to botengine.ai',
       challengeResponse,
     });
+
     return res.end(challengeResponse);
   }
+
+  // Request method is not GET, therefore this is not a verification request.
+  // Thus, we forward the webhook payload to the zapier webhook url configured
+  // in botengine.ai and passed to us as a header.
+  console.log({
+    params,
+    body,
+  });
+  return res.end('"Death"');
 }
